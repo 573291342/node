6eeb61b00775305792ce84093a43ec40941fab7b
573291342 <573291342@qq.com>
2022-10-21 13:00:37 +0800

    01.readFile的使用

--- 01.readFile的使用/01.使用readfile方法读取文件内容.js
+++ 01.readFile的使用/01.使用readfile方法读取文件内容.js
@@ -1,10 +1,16 @@
-//导入fs模块
+//1.导入fs模块，来操作文件
 const fs = require('fs')
-// 三个参数 1.路径 2.字符编码 3.回调函数
+
+//2. 调用fs.readFile()方法读取文件
+//  参数1：读取文件的存放路径
+//  参数2：读取文件时候采用的编码格式，一般默认指定utf8
+//  参数3：回调函数，拿到读取失败的结果 err dataStr
 fs.readFile('./file/1.txt', 'utf8', function (err, dataStr) {
+  // 2.1 打印失败的结果
+  // 如果读取成功，则err 的值为 null
+  // 如果读取失败，则err 的值为 错误对象，dataStr 的值为 undefined
     console.log(err);
-    console.log('-------');
-    //读取失败dataStr的值为undefined err的值为错误对象
-    //读取成功 err的值为 err
+  console.log('-------');
+  // 2.2打印成功的结果
     console.log(dataStr);
 })
